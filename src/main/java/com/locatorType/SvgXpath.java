6f495134858d2c764b04d4f65b4de730db77ea64
guneetgarg <guneetgarg@gmail.com>
2017-01-18 17:27:24 +0530

    svg locator

--- src/main/java/com/locatorType/SvgXpath.java
+++ src/main/java/com/locatorType/SvgXpath.java
@@ -1,12 +1,29 @@
 package com.locatorType;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SvgXpath {
+	WebDriver driver;
+	String path = "file:///C:/Users/ggarg1/git/CoreJavaSelenium/src/main/java/com/locatorType/SvgDemo.html";
 
 	@Test
 	private void dmo() {
-		String path = "file:///C:/Users/ggarg1/git/CoreJavaSelenium/src/main/java/com/locatorType/SvgDemo.html";
+		System.setProperty("webdriver.chrome.driver", "Drivers_executable/chromedriver.exe");
+		driver = browserStart();
+		System.out.println(driver.findElement(By.tagName("svg")).getAttribute("width"));
+		
+	}
 
+	public WebDriver browserStart() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(path);
+		return driver;
 	}
 }
