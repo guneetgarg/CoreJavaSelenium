0097c832ac50a92b3a9aa3330c46ad829cf50b77
guneetgarg <guneetgarg@gmail.com>
2017-01-18 17:10:40 +0530

    svg locator

---
+++ src/main/java/com/locatorType/SvgXpath.java
@@ -0,0 +1,12 @@
+package com.locatorType;
+
+import org.testng.annotations.Test;
+
+public class SvgXpath {
+
+	@Test
+	private void dmo() {
+		String path = "file:///C:/Users/ggarg1/git/CoreJavaSelenium/src/main/java/com/locatorType/SvgDemo.html";
+
+	}
+}
