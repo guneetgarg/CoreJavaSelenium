6c5816b63513d5ff1dcc4c26836b78028739801e
guneetgarg <guneetgarg@gmail.com>
2017-01-18 15:25:36 +0530

    Collection Prac

---
+++ src/main/java/com/collectionFrame/MapHashMap.java
@@ -0,0 +1,42 @@
+package com.collectionFrame;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MapHashMap {
+	public static void main(String[] args) {
+		HashMap<String, Double> map = new HashMap<String, Double>();
+
+		// Adding some elements to HashMap
+		map.put("Ashwin", 87.55);
+		map.put("Bharat", 95.65);
+		map.put("Chetan", 68.13);
+		map.put("Dhanjay", 74.23);
+		map.put("Kartik", 65.42);
+
+		// HashMap can have one null key and multiple null values
+		map.put(null, null);
+		map.put("Sandesh", null);
+
+		// Getting the size of the map
+		System.out.println("Size Of The Map : " + map.size());
+		System.out.println("-----------------");
+
+		Set<String> set = map.keySet();
+		Iterator<String> itr = set.iterator();
+		while (itr.hasNext()) {
+			Object key = itr.next();
+			System.out.println(key + "-->" + map.get(key));
+		}
+		System.out.println("-----------------");
+
+		// Checking the map for a particular key/value
+		System.out.println("Does this map has Chetan as key? " + map.containsKey("Chetan"));
+		System.out.println("Does this map has 74.23 as value? " + map.containsValue(74.23));
+		System.out.println("-----------------");
+
+		// Removing an element from the map
+		System.out.println("Value removed from the map : " + map.remove("Kartik"));
+	}
+}
