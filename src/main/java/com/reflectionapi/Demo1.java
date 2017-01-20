444b2d62c4cbf06ac29d73bac7dd631cbb343382
guneetgarg <guneetgarg@gmail.com>
2017-01-20 15:05:43 +0530

    Reflection demo

--- src/main/java/com/reflectionapi/Demo1.java
+++ src/main/java/com/reflectionapi/Demo1.java
@@ -4,23 +4,25 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Demo1 {
-	
+
 	public static Keywords keywords;
 	public static Method method[];
 	public static Method capturescreenShot_method;
 
-	
-	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
 		keywords = new Keywords();
 		method = keywords.getClass().getMethods();
-		for(Method a :  method){
-			//System.out.println(a);
+		for (Method a : method) {
+			// System.out.println(a);
 		}
-	//	System.out.println(method[2]);
-		
+		// System.out.println(method[2]);
+		System.out.println(method.length);
+
 		capturescreenShot_method = keywords.getClass().getMethod("captureScreenshot", String.class, String.class);
+		System.out.println(capturescreenShot_method.invoke(keywords, "",""));
 		System.out.println(capturescreenShot_method);
-		
-		method[2].invoke(keywords, "aa","");
+
+		method[2].invoke(keywords, "aa", "");
 	}
 }
