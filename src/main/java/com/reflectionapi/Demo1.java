28cfe91c891541daf2dcd6ba631362104156e854
guneetgarg <guneetgarg@gmail.com>
2017-01-20 12:55:53 +0530

    Reflection demo

---
+++ src/main/java/com/reflectionapi/Demo1.java
@@ -0,0 +1,26 @@
+package com.reflectionapi;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Demo1 {
+	
+	public static Keywords keywords;
+	public static Method method[];
+	public static Method capturescreenShot_method;
+
+	
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		keywords = new Keywords();
+		method = keywords.getClass().getMethods();
+		for(Method a :  method){
+			//System.out.println(a);
+		}
+	//	System.out.println(method[2]);
+		
+		capturescreenShot_method = keywords.getClass().getMethod("captureScreenshot", String.class, String.class);
+		System.out.println(capturescreenShot_method);
+		
+		method[2].invoke(keywords, "aa","");
+	}
+}
---
+++ src/main/java/com/reflectionapi/Keywords.java
@@ -0,0 +1,73 @@
+package com.reflectionapi;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.asserts.SoftAssert;
+
+public class Keywords {
+	public static SoftAssert s_assert = new SoftAssert();
+	public WebDriver driver;
+
+	public void openBrowser(String object, String data) {
+		System.out.println("Drivers path");
+		String path = "";
+
+	}
+
+	public void verifyTitle(String object, String data) {
+	}
+
+	public void exist(String object, String data) {
+	}
+
+	public void isErrPresent(String object, String data) {
+	}
+
+	public void click(String object, String data) {
+	}
+
+	public void executeWithJavaScript(String object, String data) {
+	}
+
+	public void synchronize(String object, String data) {
+	}
+
+	public void waitForElementVisibility(String object, String data) {
+		System.out.println("here");
+	}
+
+	public void closeBrowser(String object, String data) {
+
+	}
+
+	public void pause(String object, String data) throws NumberFormatException, InterruptedException {
+	}
+
+	/************************
+	 * APPLICATION SPECIFIC KEYWORDS
+	 ********************************/
+	public void Ribonverification(String object, String data) {
+
+	}
+
+	public void offeringCount(String object, String data) {
+	}
+
+	public void waitText(String object, String data) {
+		try {
+			Thread.sleep(3000);
+		} catch (Exception ex) {
+		}
+	}
+
+	// not a keyword
+
+	public void captureScreenshot(String filename, String keyword_execution_result){
+		System.out.println("sreen");
+		// take screen shots
+	}
+
+	public void ASSERALL() {
+		s_assert.assertAll();
+	}
+
+}
